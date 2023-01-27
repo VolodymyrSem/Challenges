be74c5d88f7e2b6dc88353536c97145a1910c843
VolodymyrSem <semenchuk.volodymyr@gmail.com>
2023-01-27 17:36:24 +0100

    Added descriptions for the tasks

--- dec_to_hex_ascii.py
+++ dec_to_hex_ascii.py
@@ -1,3 +1,7 @@
+# You need to write a function in Python that accepts a string of ASCII characters.
+# It should return each character’s value as a hexadecimal string.
+# Separate each byte by a space, and return all alpha hexadecimal characters as lowercase.
+
 def dec_to_hex(text):
     result = ''
     for el in text:
--- parse_encoded_string.py
+++ parse_encoded_string.py
@@ -1,3 +1,12 @@
+# Values in the string can be separated by any number of zeros.
+# The id is a numeric value but will contain no zeros.
+# The function should parse the string
+# and return a Python dictionary that contains the first name, last name, and id values.
+#
+# An example input would be “Robert000Smith000123”.
+# The function should return the following using that input:
+# { “first_name”: “Robert”, “last_name”: “Smith”, “id”: “123” }
+
 def parse_str(text):
     string_dict = {
         "first_name": "",
